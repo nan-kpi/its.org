23606f4a399359c1afc92f45d148b8acb40b34bf
nan-kpi <52971880+nan-kpi@users.noreply.github.com>
2019-07-23 11:47:37 +0300

    Add files via upload

--- chart1.js
+++ chart1.js
@@ -1,57 +1,63 @@
+/* global AbstractPrimaryView, _ */
 
-var AppView = Backbone.View.extend({
-  el: '#container',
+var NewChartView = AbstractPrimaryView.extend({
+    id: 'linechart',
+    tagName: 'canvas',
+    className: 'viewport linechart',
+    render: function () {
+        var datasets = {};
 
-  initialize: function() {
-    this.render();
-  },
-  render:  function Diagram () {
-  var ctx = document.getElementById("myChart");
-  var myChart = new Chart (ctx, {
-   type: 'line',
-   data: {
-    labels: [], 
-    datasets: [
-     {
-      label: 'Parabola', 
-      data: [], 
-      borderColor: 'red', 
-      borderWidth: 3, 
-      fill: false 
-     }
-  
-      ]
-    },
-   options: {
-    responsive: false, 
-    scales: {
-     xAxes: [{
-      display: true
-     }],
-     yAxes: [{
-      display: true,
-       ticks: {
-        max: 30,
-        min: 0,
-        stepSize: 1
+        _.each(this.model.getCurrentNodes(), function (node, idx) {
+            _.each(node.data, function(datum){
+                if(!_.has(datasets, datum.tclass)) {
+                    datasets[datum.tclass] = {
+                        label: datum.tclass,
+                        data: [],
+                        borderColor: 'blue', //random
+                        borderWidth: 5,
+                        fill: false
+                    };
                 }
-     }]
+                
+                if(!datum.isNumeric()) {
+                    datasets[datum.tclass].data.push(null);
+                } else {
+                    datasets[datum.tclass].data.push(datum.getValue());
+                }
+            });
+        });
+
+
+        var myChart = new Chart(this.el, {
+            type: 'line',
+            data: {
+                labels: _.pluck(this.model.getCurrentNodes(), 'name'),
+                datasets: _.toArray(datasets)
+            },
+            options: {
+                responsive: true,
+                legend: {
+                    display: false,
+                    position: 'bottom',
+                    fullWidth: false
+                },
+                scales: {
+                    xAxes: [{
+                        display: true
+                    }],
+                    yAxes: [{
+                        display: true
+                    }]
+                }
+            }
+        });
+
+        this.resizeContent();
+        myChart.update();
+
+        this.hideLoader();
+        function f(x) {
+            return Math.pow(x, 2);
+        }
     }
-   }
-  });
-
-  for (var x = -10; x<=10; x++) {
-   myChart.data.labels.push(''+x.toFixed(1));
-   myChart.data.datasets[0].data.push(f(x).toFixed(1));
-  }
-
-  myChart.update();
-
-  function f(x) { 
-   return Math.pow(x,2);
-  }
- }
-
-});
-
-var appView = new AppView();
+}); 
