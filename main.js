b0426ec4c9c11727e2a1d02c3e25c139d1ee794c
nan-kpi <52971880+nan-kpi@users.noreply.github.com>
2019-07-24 12:45:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ new GraphView({options: this.options}),
 new PrismView({options: this.options}),
 new MapView ({options: this.options}),
 //Ваше нове представлення
-new NewChartView({options: this.options}),
+new XChartView({options: this.options}),
 new YChartView({options: this.options}),
 new ZChartView({options: this.options})
 //new SmahliukChartView({options: this.options})
