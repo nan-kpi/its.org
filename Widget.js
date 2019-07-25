f76a0a1c2d80b1cb3739a3b90c0e2f5bd9d5909f
nan-kpi <52971880+nan-kpi@users.noreply.github.com>
2019-07-25 10:14:32 +0300

    Add files via upload

---
+++ Widget.js
@@ -0,0 +1,41 @@
+/* global AbstractPrimaryView, _, GraphMixin, GraphSelectionMixin */
+
+var GraphView = AbstractPrimaryView.extend({
+    id: 'graph',
+    className: 'viewport graph1',
+    contentParentSelector: null,
+    widgetTemplate: 'GraphViewWidget',
+    events: {
+        'click .btn-box-tool': function() {
+            this.$('.left-widget').toggleClass('collapsed-box');
+        },
+        'click .for-selected': function() {
+            if(this.currentSelection.length == 0) return;
+            
+            this.trigger('filterHierarchy', this.currentSelection[0].sourceModel);
+        },
+        'change select,input': function() {
+            if(this.model.isHierarchyFilter() && !this.model.isPropertyFilter()) {
+                this.render();
+                this.model.trigger('refresh');
+            }
+        }
+    },
+    initialize: function(options) {
+        AbstractPrimaryView.prototype.initialize.call(this, options);
+
+        this.initializeGraph();
+        this.$el.append(this.widgetTemplate({options: this.options.getOptionMask('graph')}));
+        this.options.translation.translateNodes(this.$el);
+    },
+    renderContentArguments: function() {
+        return this.model.getCurrentNodes({
+            direction: this.$('[name="filterDirection"]').val(),
+            depth: this.$('[name="filterDepth"]').val()
+        });
+    },
+    getCurrentNodeCount: function() {
+        return this.graph.graph.nodes().length;
+    },
+    mixins: [GraphSelectionMixin, GraphMixin]
+});
