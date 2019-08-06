242745c3191d4c4fdee34b23843fd91325899d30
nan-kpi <52971880+nan-kpi@users.noreply.github.com>
2019-08-06 14:47:48 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -6,7 +6,7 @@ var YChartView = AbstractPrimaryView.extend({
         
      
      
-var g = new JSGantt.GanttChart('g',document.getElementById('GanttChartDIV'), 'day');
+var g = new JSGantt.GanttChart('g',document.getElementById('linechart2'), 'day');
   g.setShowRes(1); // Show/Hide Responsible (0/1)
   g.setShowDur(1); // Show/Hide Duration (0/1)
   g.setShowComp(1); // Show/Hide % Complete(0/1)
