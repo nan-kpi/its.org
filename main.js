d48f6949ffbd809d5a9a5996d8fd30f8994653a9
nan-kpi <52971880+nan-kpi@users.noreply.github.com>
2019-08-06 13:41:15 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,11 @@
+var SongView = Backbone.View.extend({
 
-jQuery(document).on('todos.initialize', function (evt, that) {
-_.extend(that, {
-resolvePrimaryViews: function () {
-return [
-//стандартні представлення
-new TileView ({options: this.options}),
-new TableView({options: this.options}),
-new GraphView({options: this.options}),
-new CustomGraphView({options: this.options}),
-new PrismView({options: this.options}),
-new MapView ({options: this.options}),
-//Ваше нове представлення
+    render:function(){
+        this.$el.html("Hello World!");
 
-new XChartView({options: this.options}),
-new YChartView({options: this.options}),
-new ZChartView({options: this.options})
-];
-},
+        return this;
+    }
 });
-});
+
+var songView = new SongView({el: "#container" });
+songView.render();
