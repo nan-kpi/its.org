de51b9ab1085af04f45d72be8702d0f008fa9910
nan-kpi <52971880+nan-kpi@users.noreply.github.com>
2019-08-06 14:16:58 +0300

    Update xxxchart2.js

--- xxxchart2.js
+++ xxxchart2.js
@@ -73,7 +73,13 @@ var YChartView = AbstractPrimaryView.extend({
                 }
             }
         });
+    this.resizeContent();
+        myChart.update();
 
+        this.hideLoader();
+        function f(x) {
+            return Math.pow(x, 2);
+        }
       
     }
 }); 
