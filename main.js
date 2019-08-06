6315897bc2b2b4b0af8e7568a4779730e67e2380
nan-kpi <52971880+nan-kpi@users.noreply.github.com>
2019-08-06 13:45:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ new MapView ({options: this.options}),
 new XChartView({options: this.options}),
 new YChartView({options: this.options}),
 new ZChartView({options: this.options})
+//new SmahliukChartView({options: this.options})
 ];
 },
+});
 });
