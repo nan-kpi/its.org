8b71b4e2aea79cdc4e6acddd3d6527ce35762a57
nan-kpi <52971880+nan-kpi@users.noreply.github.com>
2019-08-14 21:39:47 +0300

    Update chart1.js

--- chart1.js
+++ chart1.js
@@ -8,7 +8,7 @@ var XChartView = AbstractPrimaryView.extend({
 //    contentTemplate: 'ChartMain',
     events: {
         'click .btn-box-tool': function() {
-            this.$('.left-widget').toggleClass('.collapsed-box');
+             this.$('.left-widget').toggleClass('collapsed-box');
         },
             
         'click input[type=checkbox]': function() {
