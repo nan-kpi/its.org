60dc2f21a659930645a67c313ea7e2a77eff8c10
nan-kpi <52971880+nan-kpi@users.noreply.github.com>
2019-08-17 20:01:06 +0300

    Update help_with_DataSet.js

--- help_with_DataSet.js
+++ help_with_DataSet.js
@@ -40,30 +40,7 @@ var YChartView = AbstractPrimaryView.extend({
                 /*********************************************************************/
 
                 var datasets = {};
-        var myDataSet = [];
-           myDataSet = [
-            ['058467', 'Побудова Стадіону Голінський', 'Процес', new Date(2019, 2, 1), new Date(2020, 2, 11), null, 78, null],
-            ['000001', 'Пошук альтернатив', 'вфіаіфва', new Date(2019, 2, 1), new Date(2019, 7, 10), null, 78, null],
-            ['000002', 'Тестовий процес', '4546156', new Date(2019, 2, 1), new Date(2019, 6, 11), null, 78, null],
         
-            ['071489','Виділення земельної ділянки','Територія',new Date(2019, 2, 1),new Date(2019, 2, 30) ,null,100, null],
-            
-            ['074759','Оголошення тендеру','Тендер', new Date(2019, 2, 5),new Date(2019, 2, 16) ,null,100, null],
-            ['074760','Реєстрація пропозицій','Тендер', new Date(2019, 2, 16),new Date(2019, 2, 25) ,null,100, '074759'],
-            ['074761','Перевірка ліцензій','Тендер', new Date(2019, 2, 23),new Date(2019, 2, 27) ,null,100, '074759'],
-            ['074762','Огляд витрат','Тендер', new Date(2019, 2, 27),new Date(2019, 3, 10) ,null,100, '074759'],
-            ['074763','Укладення договору','Тендер', new Date(2019, 3, 10),new Date(2019, 3, 16) ,null,100, '074759'],
-            
-            ['086457','Закупівля матеріалів','Матеріали', new Date(2019, 4, 1),new Date(2019, 6, 10) ,null,80, '074763'],
-            
-            ['097458','Фасад','Побудова стадіону', new Date(2019, 5, 26),new Date(2019, 8, 10) ,null,88, '086457'],
-            ['097459','Дах','Побудова стадіону', new Date(2019, 7, 26),new Date(2019, 12, 10) ,null,90, '086457'],
-            ['097461','Ліве крило','Побудова стадіону', new Date(2019, 8, 10),new Date(2019, 8, 26) ,null, 82, '086457'],
-            ['097462','Праве крило','Побудова стадіону', new Date(2019, 9, 26),new Date(2019, 11, 10) ,null,35, '086457'],
-            ['097463','Кінцеві роботи','Побудова стадіону', new Date(2020,0, 15),new Date(2020, 1,28) ,null,60, '086457'],
-            ['097460','Контроль термінів будування','Побудова стадіону', new Date(2020,1, 28),new Date(2020, 2, 6) ,null,75, '086457'],
-            ['125465','Відкриття стадіону','Заходи', new Date(2020,2, 10),new Date(2020, 2, 11) ,null,50, '086457'],
-        ];
         
          var testDataSets = [];
         
@@ -86,35 +63,35 @@ _.each(list, iteratee, [context]) - Проходит по всему списк�
                 }
                 
                if(datum.tclass == "Task ID") {
-                    testDataSets[0].push(datum.getValue()); //Вставити у відповідне місце у масиві
+                    this.testDataSets[0].push(datum.getValue()); //Вставити у відповідне місце у масиві
                 } else
                 if(datum.tclass == "Task Name") {
                     //datasets[datum.tclass].data.push(datum.getValue()); //Вставити у відповідне місце у масиві
-                    testDataSets[1].push(datum.getValue());
+                    this.testDataSets[1].push(datum.getValue());
                 } else
                 if(datum.tclass == "Resource") {
                     //datasets[datum.tclass].data.push(datum.getValue()); //Вставити у відповідне місце у масиві
-                    testDataSets[2].push(datum.getValue());
+                   this.testDataSets[2].push(datum.getValue());
                 } else
                 if(datum.tclass == "Start Date") {
                     //datasets[datum.tclass].data.push(datum.getValue()); //Вставити у відповідне місце у масиві
-                    testDataSets[3].push(datum.getValue());
+                    this.testDataSets[3].push(datum.getValue());
                 } else
                 if(datum.tclass == "DeadLine") {
                     //datasets[datum.tclass].data.push(datum.getValue()); //Вставити у відповідне місце у масиві
-                    testDataSets[4].push(datum.getValue());
+                    this.testDataSets[4].push(datum.getValue());
                 } else
                 if(datum.tclass == "Duration") {
                     //datasets[datum.tclass].data.push(datum.getValue()); //Вставити у відповідне місце у масиві
-                    testDataSets[5].push(datum.getValue());
+                    this.testDataSets[5].push(datum.getValue());
                 } else
                 if(datum.tclass == "Percent Complete") {
                     //datasets[datum.tclass].data.push(datum.getValue()); //Вставити у відповідне місце у масиві
-                    testDataSets[6].push(datum.getValue());
+                    this.testDataSets[6].push(datum.getValue());
                 } else
                 if(datum.tclass == "Dependencies") {
                     //datasets[datum.tclass].data.push(datum.getValue()); //Вставити у відповідне місце у масиві
-                    testDataSets[7].push(datum.getValue());
+                    this.testDataSets[7].push(datum.getValue());
                 }else {
                     datasets[datum.tclass].data.push(10); //Останній варіант - сповістить про помилку введення
                 }
@@ -137,7 +114,7 @@ _.each(list, iteratee, [context]) - Проходит по всему списк�
                 Data.addColumn('number', 'Percent Complete');
                 Data.addColumn('string', 'Dependencies');
                 
-                /*myDataSet = [
+                myDataSet = [
             ['058467', 'Побудова Стадіону Голінський', 'Процес', new Date(2019, 2, 1), new Date(2020, 2, 11), null, 78, null],
             ['000001', 'Пошук альтернатив', 'вфіаіфва', new Date(2019, 2, 1), new Date(2019, 7, 10), null, 78, null],
             ['000002', 'Тестовий процес', '4546156', new Date(2019, 2, 1), new Date(2019, 6, 11), null, 78, null],
@@ -159,7 +136,7 @@ _.each(list, iteratee, [context]) - Проходит по всему списк�
             ['097463','Кінцеві роботи','Побудова стадіону', new Date(2020,0, 15),new Date(2020, 1,28) ,null,60, '086457'],
             ['097460','Контроль термінів будування','Побудова стадіону', new Date(2020,1, 28),new Date(2020, 2, 6) ,null,75, '086457'],
             ['125465','Відкриття стадіону','Заходи', new Date(2020,2, 10),new Date(2020, 2, 11) ,null,50, '086457'],
-        ];*/
+        ];
                
               for(var i = 0; i < myDataSet.length; i++){
             myDataSet[i][2] = this.DeadLine(myDataSet[i][2], myDataSet[i][4],  myDataSet[i][6]);
@@ -168,7 +145,7 @@ _.each(list, iteratee, [context]) - Проходит по всему списк�
              
                 //testDataSets
                // Data.addRows(myDataSet);
-                Data.addRows(testDataSets);
+                Data.addRows(this.testDataSets);
 
                    var options = {
                backgroundColor: {
