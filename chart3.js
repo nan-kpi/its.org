73e0657d6e234d5e15208446a2f2887a60c33626
nan-kpi <52971880+nan-kpi@users.noreply.github.com>
2019-08-21 13:19:51 +0300

    Update chart3.js

--- chart3.js
+++ chart3.js
@@ -51,8 +51,8 @@ this.$el.append(this.widgetTemplate(this.renderWidgetArguments(datasets)));
         var myChart = new Chart(this.el, {
             type: 'radar',
             data: {
-                labels: _.pluck(datasets),
-                datasets: _.toArray(this.model.getCurrentNodes(), 'name')
+                labels: _.pluck(this.model.getCurrentNodes(), 'name'),
+                datasets: _.toArray(datasets)
             },
             options: {
                 responsive: true,
